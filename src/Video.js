1d7878b6b8645c083c9a6cb566793f6ce7371b41
dixroby arone cuellar <alestilodefe@gmail.com>
2020-09-04 20:59:20 -0500

    Feat: Tik Tok frontend Completed.
    Diseño de Tik Tok parte frontend Funcional.

---
+++ src/Video.js
@@ -0,0 +1,44 @@
+import React ,{ useRef, useState } from 'react'
+import "./video.css"
+import VideoFooter from './VideoFooter';
+import VideoSidebar from './VideoSidebar';
+
+function Video({url,channel,description,likes,shares,messages,song}) {
+    const [playing,setPlaying]=useState(false);
+  const videoRef =  useRef(null);
+  const handleVideoPress = ()=>{
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    }else{
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  }
+    return (
+        <div className="video">
+            <video 
+                onClick={handleVideoPress}
+                ref={videoRef}
+                className="video__player"
+                loop
+                src={ url}>
+
+            </video>
+
+            {/* Video footer */}  
+            <VideoFooter 
+              channel={channel}
+              description={description}
+              song={song}
+            />
+            <VideoSidebar 
+            likes={likes} 
+            shares={shares} 
+            messages={messages}/>
+            {/* Video Sidebar */}
+        </div>
+    )
+}
+
+export default  Video;
